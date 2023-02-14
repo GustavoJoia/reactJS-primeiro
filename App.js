36bce9e4b6f1f50ed186414c33b0a21ee3deb98b
unknown <gustavo.joia57@gmail.com>
2023-02-14 10:38:34 -0300

    Adição de textos-teste e botões

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text> 
+      <Text>blá blá bláblá blá bláblá blá bláblá blá blá</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="enviar">Enviar</Button>
       <StatusBar style="auto" />
     </View>
   );
